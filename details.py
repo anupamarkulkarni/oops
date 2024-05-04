17680f99cd15c645f24c61bc610129b5330068d4
anupamarkulkarni <167953646+anupamarkulkarni@users.noreply.github.com>
2024-05-04 09:29:04 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,22 @@
+from Abstractdetails import Abstractdetails
+
+class keerthi(Abstractdetails):
+    def display_details(self):
+        print("keerthi Details:")
+        print("bank:", self.bank)
+        print("Number of pin:", self.num_pin)
+        print("password:", self.password)
+
+class anu(Abstractdetails):
+    def display_details(self):
+        print("anu Details:")
+        print("bank:", self.bank)
+        print("Number of pin:", self.num_pin)
+        print("password:", self.password)
+
+class ayesha(Abstractdetails):
+    def display_details(self):
+        print("ayesha Details:")
+        print("bank:", self.bank)
+        print("Number of pin:", self.num_pin)
+        print("password:",self.password)
