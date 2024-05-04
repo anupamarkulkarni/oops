2ca653913c0bee63797fb39ed777a8dfe807c2aa
anupamarkulkarni <167953646+anupamarkulkarni@users.noreply.github.com>
2024-05-04 09:38:36 +0530

    Add files via upload

---
+++ overriding.py
@@ -0,0 +1,10 @@
+class Animal:
+    def speak(self):
+        return "Animal makes a sound"
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+    
