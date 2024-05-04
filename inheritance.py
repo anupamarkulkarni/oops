c020f36423f589376aea94a252c89ff1279fc710
anupamarkulkarni <167953646+anupamarkulkarni@users.noreply.github.com>
2024-05-04 09:35:30 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,15 @@
+class human:
+    def __init__(self,hobby,food):
+        self.fav_hobby=hobby
+        self.fav_food=food
+    def display_info(self):
+        print(f"He plays {self.fav_hobby} and he likes {self.fav_food}")
+class sanjay(human):
+    def __init__(self,hobby,food,language):
+        super().__init__(hobby,food)
+        self.fav_language=language
+    def display_info(self):
+        super().display_info()
+        print(f"Language he speaks {self.fav_language}")
+user = sanjay("Games","Biriyani","Tamil")
+user.display_info()
